a62bbde78ea9b8931856ea4b3e66702e1fe3211f
MullaN <admullan@gmail.com>
2021-03-24 19:37:48 -0700

    added logic to countdown to show seconds when under one day

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -5,7 +5,7 @@ const Countdown = () => {
     const timeLeft = () => {
         let startTime = new Date()
         let oscarTime = new Date('April 25 2021 17:00:00 UTC-0700')
-        let timeBetween = oscarTime - startTime
+        let timeBetween = oscarTime - startTime + 1000
         let days = Math.floor(timeBetween / 1000 / 60 / 60 / 24)
         let hours = Math.floor(timeBetween / 1000 / 60 / 60 % 24)
         let minutes = Math.floor(timeBetween / 1000 / 60 % 60)
@@ -21,21 +21,39 @@ const Countdown = () => {
         return () => clearTimeout(timeout)
     })
 
-    return(
-        <Grid container className='countdown' spacing={2} justify='center'>
-            <Grid item>
-                <span className="countdown-number">{countdown.days}</span><br />
-                DAYS
-            </Grid>
-            <Grid item>
-                <span className="countdown-number">{countdown.hours}</span><br />
-                HOURS
-            </Grid>
-            <Grid item>
-                <span className="countdown-number">{countdown.minutes}</span><br />
-                MINUTES
-            </Grid>
-        </Grid>
+    return( 
+            countdown.seconds > 0 ?
+                <Grid container className='countdown' spacing={4} justify='center'>
+                    {countdown.days >= 1 ?                     
+                        <Grid item>
+                            <span className="countdown-number">{countdown.days}</span><br />
+                            DAYS
+                        </Grid>
+                        :
+                        <></>
+                    }
+                    <Grid item>
+                        <span className="countdown-number">{countdown.hours}</span><br />
+                        HOURS
+                    </Grid>
+                    <Grid item>
+                        <span className="countdown-number">{countdown.minutes}</span><br />
+                        MINUTES
+                    </Grid>
+                    {countdown.days < 1 ? 
+                        <Grid item>
+                        <span className="countdown-number">{countdown.seconds}</span><br />
+                        SECONDS
+                        </Grid>
+                        :
+                        <></>}
+                </Grid>
+            :
+            countdown.hours > -4 ? 
+            <span id='live-text'>OSCARS ARE LIVE!</span>
+            :
+            <></>
+            
     )
 }
 
