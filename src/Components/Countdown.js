1be01903c7e84c1c448589d4fc6c845c82809633
MullaN <admullan@gmail.com>
2022-02-17 18:18:20 -0800

    updated for 2022

--- src/Components/Countdown.js
+++ src/Components/Countdown.js
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid'
 const Countdown = () => {
     const timeLeft = () => {
         let startTime = new Date()
-        let oscarTime = new Date('April 25 2021 17:00:00 UTC-0700')
+        let oscarTime = new Date('March 27 2022 17:00:00 UTC-0700')
         let timeBetween = oscarTime - startTime + 1000
         let days = Math.floor(timeBetween / 1000 / 60 / 60 / 24)
         let hours = Math.floor(timeBetween / 1000 / 60 / 60 % 24)
